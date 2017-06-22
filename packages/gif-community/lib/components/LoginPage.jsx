519c601206dae62584215c350496404f29d3f107
codeluggage <matias.forbord@gmail.com>
2017-06-22 00:59:34 -0400

    Style log in page with logo

--- packages/gif-community/lib/components/LoginPage.jsx
+++ packages/gif-community/lib/components/LoginPage.jsx
@@ -11,11 +11,28 @@ const NavLoggedOut = ({currentUser}) => (
   <div className="header-logged-out" style={{
       display: 'flex',
       justifyContent: 'center',
+      textAlign: 'center',
+      flexDirection: 'column'
   }}>
 
+      <h1 style={{
+        flex: 1,
+        textAlign: 'center',
+      }}>
+        <img style={{
+          width: '150px',
+          height: '150px',
+        }} src="https://res.cloudinary.com/shortcutmagic/image/upload/v1498105564/wizard_x7n0am.png" alt="GifCommunity"/>
+      </h1>
+
     <div style={{
+      display: 'flex',
+      flex: 1,
       marginTop: '20px',
+      justifyContent: 'center',
+      flexDirection: 'column'
     }}>
+
 
       <div style={{
         marginBottom: '20px',
