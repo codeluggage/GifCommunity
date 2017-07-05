20794a72c92e16494f321425d56a3bdc3c75ff61
codeluggage <matias.forbord@gmail.com>
2017-07-04 22:11:25 -0400

    Unify flow

--- packages/gif-community/lib/components/WelcomeFlow.jsx
+++ packages/gif-community/lib/components/WelcomeFlow.jsx
@@ -9,11 +9,17 @@ const WelcomeFlow = ({results = [], currentUser, loading, loadMore, count, total
     alignContent: 'center',
     textAlign: 'center',
   }}>
-
     {loading ? 
 
       <Loading /> :
-      <div>
+
+      <div style={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+      }}>
         <h2>Let's Install ShortcutMagic</h2>
         <br />
         Installing ShortcutMagic is easy, but it requires administrative access. 
@@ -24,10 +30,31 @@ const WelcomeFlow = ({results = [], currentUser, loading, loadMore, count, total
         If something does not work, please send en email to matias@shortcutmagic.com so I can help! 
         <br />
         <br />
-        <a className="btn btn-primary" href="/welcome-permissions">Continue</a>
+
+        <h2>Administrative access</h2>
+        <br />
+        ShortcutMagic works very closely with your computer. These kinds of programs are often 
+        <br />
+        called assistive access programs. They can control limited parts of the computer for you,
+        <br />
+        like clicking buttons or typing by voice. ShortcutMagic interacts with your programs for you,
+        <br />
+        and to do all of that it needs administrative access. 
+        <br />
+        <br />
+        Here is a gif showing what will happen:
+        <br />
+        <img style={{
+          width: '450px',
+          height: '450px',
+        }} src="https://res.cloudinary.com/shortcutmagic/image/upload/v1498105564/wizard_x7n0am.png" alt="GifCommunity"/>
+        <br />
+        <br />
+        <br />
+
       </div>
     }
-
   </div>
 
+  
 export default withCurrentUser(WelcomeFlow);
